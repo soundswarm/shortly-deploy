5b055484e94af5432dcb86bac876da57d5abe96f
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 17:28:19 -0700

    deleted some comments

--- app/models/link.js
+++ app/models/link.js
@@ -20,28 +20,3 @@ linkSchema.pre('save', function(next){
 var Link = mongoose.model('Link', linkSchema);
 module.exports = Link;
 
-// links
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
-
--- app/models/user.js
+++ app/models/user.js
@@ -28,27 +28,3 @@ User.comparePassword = function(attemptedPassword, savedPassword, callback) {
 module.exports = User;
 
 
-
-//
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).birs
-//     nd(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
-
-

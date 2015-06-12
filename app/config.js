a0747e6fc4885f97517d5c02b50121b0c2e86da2
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 19:00:35 -0700

    ...

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ var port = require('../server.js')
 // var path = require('path');
 var mongoose = require('mongoose');
 
-var mongoUrl = 'mongodb://@ds036638.mongolab.com:36638/MongoLab-r'//process.env.CUSTOMCONNSTR_MONGOLAB_URI ||'mongodb://127.0.0.1:27017/shortlydb';
+var mongoUrl = 'mongodb://@ds036638.mongolab.com:36638/MongoLab-r -u MongoLab-r -p'//process.env.CUSTOMCONNSTR_MONGOLAB_URI ||'mongodb://127.0.0.1:27017/shortlydb';
 
 mongoose.connect(mongoUrl);
 var db = mongoose.connection;
